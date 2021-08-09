bb9cb83b63542dbf3591b57e7c864735b3b08ccd
JesusLeto <cghplus651@gmail.com>
2021-08-09 11:01:39 +0300

    Final

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ThemeContext from "./context";
+
+// localStorage.setItem("Table", JSON.stringify([[null]]))
+
+const Main = () => {
+  console.log(JSON.parse(localStorage.getItem("Table")))
+  const [TableArr, setTableArr] = useState(JSON.parse(localStorage.getItem("Table")) == null ? [[null]]: JSON.parse(localStorage.getItem("Table")))
+
+  return(
+    
+    <React.StrictMode>
+      <ThemeContext.Provider value = {{TableArr, setTableArr}}>
+        <App />
+      </ThemeContext.Provider>
+  </React.StrictMode>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Main />,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
